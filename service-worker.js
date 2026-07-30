950f404a839119d189e85b73b49c28c631e73839
Oscar <oscar@vonarson.com>
2026-07-30 19:16:59 +0000

    Custom icon set, collapsible composer, compact vitals, focus split + momentum widgets

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mindorg-cache-v5";
+const CACHE_NAME = "mindorg-cache-v6";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./brain-hero.png"];
 
 self.addEventListener("install", (event) => {
