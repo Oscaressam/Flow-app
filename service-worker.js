8b7dfb293dac6741570a4e5e8edbc2e12bdcda7e
Oscar <oscar@vonarson.com>
2026-07-30 19:45:50 +0000

    Purpose-drawn notification badge: brain + circle silhouette, legible at 24dp

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mindorg-cache-v7";
+const CACHE_NAME = "mindorg-cache-v8";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./brain-hero.png", "./badge-72.png"];
 
 self.addEventListener("install", (event) => {
