bc6d24b3807733075043eae5ceeb6e18b8680b27
Oscar <oscar@vonarson.com>
2026-07-30 19:07:38 +0000

    Kinetic UI: brain hero, motion choreography, snap-scroll cover rail

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "mindorg-cache-v4";
-const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
+const CACHE_NAME = "mindorg-cache-v5";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./brain-hero.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
