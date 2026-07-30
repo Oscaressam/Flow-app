cca7d3fa8a2b63f5ba4fc66890e925a9a05a102e
Oscar <oscar@vonarson.com>
2026-07-30 18:43:12 +0000

    Amber glass UI: logo palette (white/amber/black), frosted surfaces, futuristic accents

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mindorg-cache-v3";
+const CACHE_NAME = "mindorg-cache-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
