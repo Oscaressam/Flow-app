38625959673492878c22ef70d595e6f7ac53e0bc
Oscar <oscar@vonarson.com>
2026-09-11 12:39:25 +0000

    Replace Work with Canvas category: freeform boards with draggable/resizable cards, connections, colors, multiple boards. Isolated DOM overlay so background timers can never wipe an in-progress drag.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mindorg-cache-v55";
+const CACHE_NAME = "mindorg-cache-v56";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./brain-hero.png", "./badge-72.png"];
 
 self.addEventListener("install", (event) => {
